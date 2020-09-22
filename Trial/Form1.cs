e55028d4255b374f67d3d1bb1eef2508179a474c
sameh4d4 <71703749+sameh4d4@users.noreply.github.com>
2020-09-22 18:37:40 +0700

    Add project files.

---
+++ Trial/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trial
+{
+    public partial class Form1 : Form
+    {
+        List<int> iniList = new List<int>();
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            double h = 1;
+            while (h < 100)
+            {
+                listBox1.Items.Add(h + "");
+                if (h % 20 == 0)
+                {
+                    break;
+                }
+                h++;
+            }
+        }
+    }
+}
