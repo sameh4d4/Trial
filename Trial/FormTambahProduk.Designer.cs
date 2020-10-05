a40fc3c2a824466a3e17ba4878035eca69ead856
Peter Jose <34370936+peterjosee@users.noreply.github.com>
2020-10-05 07:57:12 +0700

    nambah form

---
+++ Trial/FormTambahProduk.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Trial
+{
+    partial class FormTambahProduk
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxKode = new System.Windows.Forms.TextBox();
+            this.textBoxNama = new System.Windows.Forms.TextBox();
+            this.textBoxHarga = new System.Windows.Forms.TextBox();
+            this.textBoxStok = new System.Windows.Forms.TextBox();
+            this.buttonSimpan = new System.Windows.Forms.Button();
+            this.buttonKeluar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(345, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TAMBAH PRODUK";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBoxStok);
+            this.panel1.Controls.Add(this.textBoxHarga);
+            this.panel1.Controls.Add(this.textBoxNama);
+            this.panel1.Controls.Add(this.textBoxKode);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(12, 54);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(345, 143);
+            this.panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Kode: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Nama:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Harga:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 93);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Stok:";
+            // 
+            // textBoxKode
+            // 
+            this.textBoxKode.Location = new System.Drawing.Point(84, 12);
+            this.textBoxKode.Name = "textBoxKode";
+            this.textBoxKode.Size = new System.Drawing.Size(185, 20);
+            this.textBoxKode.TabIndex = 4;
+            // 
+            // textBoxNama
+            // 
+            this.textBoxNama.Location = new System.Drawing.Point(84, 35);
+            this.textBoxNama.Name = "textBoxNama";
+            this.textBoxNama.Size = new System.Drawing.Size(185, 20);
+            this.textBoxNama.TabIndex = 5;
+            // 
+            // textBoxHarga
+            // 
+            this.textBoxHarga.Location = new System.Drawing.Point(84, 61);
+            this.textBoxHarga.Name = "textBoxHarga";
+            this.textBoxHarga.Size = new System.Drawing.Size(185, 20);
+            this.textBoxHarga.TabIndex = 6;
+            // 
+            // textBoxStok
+            // 
+            this.textBoxStok.Location = new System.Drawing.Point(84, 87);
+            this.textBoxStok.Name = "textBoxStok";
+            this.textBoxStok.Size = new System.Drawing.Size(185, 20);
+            this.textBoxStok.TabIndex = 7;
+            // 
+            // buttonSimpan
+            // 
+            this.buttonSimpan.Location = new System.Drawing.Point(282, 203);
+            this.buttonSimpan.Name = "buttonSimpan";
+            this.buttonSimpan.Size = new System.Drawing.Size(75, 36);
+            this.buttonSimpan.TabIndex = 2;
+            this.buttonSimpan.Text = "SIMPAN";
+            this.buttonSimpan.UseVisualStyleBackColor = true;
+            // 
+            // buttonKeluar
+            // 
+            this.buttonKeluar.Location = new System.Drawing.Point(167, 203);
+            this.buttonKeluar.Name = "buttonKeluar";
+            this.buttonKeluar.Size = new System.Drawing.Size(75, 36);
+            this.buttonKeluar.TabIndex = 3;
+            this.buttonKeluar.Text = "KELUAR";
+            this.buttonKeluar.UseVisualStyleBackColor = true;
+            // 
+            // FormTambahProduk
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 266);
+            this.Controls.Add(this.buttonKeluar);
+            this.Controls.Add(this.buttonSimpan);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "FormTambahProduk";
+            this.Text = "FormTambahProduk";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBoxNama;
+        private System.Windows.Forms.TextBox textBoxKode;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxStok;
+        private System.Windows.Forms.TextBox textBoxHarga;
+        private System.Windows.Forms.Button buttonSimpan;
+        private System.Windows.Forms.Button buttonKeluar;
+    }
+}
