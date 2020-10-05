b81b12de8697763abe108f6c9f5dd77ed9bb0e69
Albert <hermanlau89@gmail.com>
2020-10-05 19:28:00 +0800

    Revert "Revert "method ubahdata hapusdata, form ubah data hapus data""
    This reverts commit 8f80cc0d283827301af905cfd1e9512007c69c2c.

--- Trial/FormUbahProduk.cs
+++ Trial/FormUbahProduk.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using ClassLibrary1;
 namespace Trial
 {
     public partial class FormUbahProduk : Form
@@ -15,6 +15,22 @@ namespace Trial
         public FormUbahProduk()
         {
             InitializeComponent();
+        }
+
+        private void buttonUbah_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                Barang b = new Barang(int.Parse(textBoxIDBarang.Text), int.Parse(textBoxIDPenjual.Text), textBoxJenisBarang.Text, textBoxNama.Text, int.Parse(textBoxStok.Text), int.Parse(textBoxHarga.Text), richTextBoxDeskripsi.Text);
+                Barang.UbahData(b);
+                MessageBox.Show("Data barang diubah.", "informasi");
+
+            }
+            catch (Exception ec)
+            {
+                MessageBox.Show("Data barang gagal diubah. Pesan Kesalahan: " + ec.Message, "kesalahan");
+            }
         }
     }
 }
