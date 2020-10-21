41bceb3aa1eb2d22f7d01c4a539a35203921ff36
Abednego Joshua <abednegojoshua_07@yahoo.co.id>
2020-10-21 14:06:08 +0700

    FormLaporanDanCetak

---
+++ Trial/FormLaporanPenjualan.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Trial
+{
+    partial class FormLaporanPenjualan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dateTimePickerEnd = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePickerStart = new System.Windows.Forms.DateTimePicker();
+            this.buttonCari = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridViewLaporan = new System.Windows.Forms.DataGridView();
+            this.buttonKeluar = new System.Windows.Forms.Button();
+            this.buttonCetak = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLaporan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel2.Controls.Add(this.dateTimePickerEnd);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.dateTimePickerStart);
+            this.panel2.Controls.Add(this.buttonCari);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(12, 84);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(833, 54);
+            this.panel2.TabIndex = 20;
+            // 
+            // dateTimePickerEnd
+            // 
+            this.dateTimePickerEnd.CustomFormat = "dd/MM/yyyy";
+            this.dateTimePickerEnd.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePickerEnd.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePickerEnd.Location = new System.Drawing.Point(457, 15);
+            this.dateTimePickerEnd.Margin = new System.Windows.Forms.Padding(2);
+            this.dateTimePickerEnd.Name = "dateTimePickerEnd";
+            this.dateTimePickerEnd.Size = new System.Drawing.Size(153, 29);
+            this.dateTimePickerEnd.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Open Sans", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(410, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 26);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "s/d";
+            // 
+            // dateTimePickerStart
+            // 
+            this.dateTimePickerStart.CustomFormat = "dd/MM/yyyy";
+            this.dateTimePickerStart.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePickerStart.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePickerStart.Location = new System.Drawing.Point(246, 15);
+            this.dateTimePickerStart.Margin = new System.Windows.Forms.Padding(2);
+            this.dateTimePickerStart.Name = "dateTimePickerStart";
+            this.dateTimePickerStart.Size = new System.Drawing.Size(153, 29);
+            this.dateTimePickerStart.TabIndex = 13;
+            // 
+            // buttonCari
+            // 
+            this.buttonCari.BackColor = System.Drawing.Color.RoyalBlue;
+            this.buttonCari.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.buttonCari.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCari.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCari.ForeColor = System.Drawing.Color.Black;
+            this.buttonCari.Location = new System.Drawing.Point(635, 16);
+            this.buttonCari.Name = "buttonCari";
+            this.buttonCari.Size = new System.Drawing.Size(70, 28);
+            this.buttonCari.TabIndex = 12;
+            this.buttonCari.Text = "CARI";
+            this.buttonCari.UseVisualStyleBackColor = false;
+            this.buttonCari.Click += new System.EventHandler(this.buttonCari_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Open Sans", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(138, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 26);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Periode:";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Gainsboro;
+            this.label1.Font = new System.Drawing.Font("Open Sans", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(833, 60);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "LAPORAN PENJUALAN";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dataGridViewLaporan
+            // 
+            this.dataGridViewLaporan.BackgroundColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle9.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            dataGridViewCellStyle9.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewLaporan.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle9;
+            this.dataGridViewLaporan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle10.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            dataGridViewCellStyle10.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle10.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle10.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle10.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewLaporan.DefaultCellStyle = dataGridViewCellStyle10;
+            this.dataGridViewLaporan.Location = new System.Drawing.Point(12, 143);
+            this.dataGridViewLaporan.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewLaporan.Name = "dataGridViewLaporan";
+            this.dataGridViewLaporan.RowHeadersWidth = 51;
+            this.dataGridViewLaporan.RowTemplate.Height = 28;
+            this.dataGridViewLaporan.Size = new System.Drawing.Size(833, 271);
+            this.dataGridViewLaporan.TabIndex = 21;
+            // 
+            // buttonKeluar
+            // 
+            this.buttonKeluar.BackColor = System.Drawing.Color.RoyalBlue;
+            this.buttonKeluar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonKeluar.Font = new System.Drawing.Font("Open Sans", 14F, System.Drawing.FontStyle.Bold);
+            this.buttonKeluar.ForeColor = System.Drawing.Color.Black;
+            this.buttonKeluar.Location = new System.Drawing.Point(725, 419);
+            this.buttonKeluar.Name = "buttonKeluar";
+            this.buttonKeluar.Size = new System.Drawing.Size(120, 39);
+            this.buttonKeluar.TabIndex = 24;
+            this.buttonKeluar.Text = "KELUAR";
+            this.buttonKeluar.UseVisualStyleBackColor = false;
+            this.buttonKeluar.Click += new System.EventHandler(this.buttonKeluar_Click);
+            // 
+            // buttonCetak
+            // 
+            this.buttonCetak.BackColor = System.Drawing.Color.RoyalBlue;
+            this.buttonCetak.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.buttonCetak.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCetak.Font = new System.Drawing.Font("Open Sans", 14F, System.Drawing.FontStyle.Bold);
+            this.buttonCetak.ForeColor = System.Drawing.Color.Black;
+            this.buttonCetak.Location = new System.Drawing.Point(615, 419);
+            this.buttonCetak.Name = "buttonCetak";
+            this.buttonCetak.Size = new System.Drawing.Size(99, 39);
+            this.buttonCetak.TabIndex = 23;
+            this.buttonCetak.Text = "CETAK";
+            this.buttonCetak.UseVisualStyleBackColor = false;
+            this.buttonCetak.Click += new System.EventHandler(this.buttonCetak_Click);
+            // 
+            // FormLaporanPenjualan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(858, 470);
+            this.Controls.Add(this.buttonKeluar);
+            this.Controls.Add(this.buttonCetak);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.dataGridViewLaporan);
+            this.Controls.Add(this.label1);
+            this.Name = "FormLaporanPenjualan";
+            this.Text = "FormLaporanPenjualan";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLaporan)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DateTimePicker dateTimePickerEnd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePickerStart;
+        private System.Windows.Forms.Button buttonCari;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridViewLaporan;
+        private System.Windows.Forms.Button buttonKeluar;
+        private System.Windows.Forms.Button buttonCetak;
+    }
+}
---
+++ Trial/FormLaporanPenjualan.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trial
+{
+    public partial class FormLaporanPenjualan : Form
+    {
+        public FormLaporanPenjualan()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCari_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCetak_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonKeluar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
