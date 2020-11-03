d5fe3af4359a0f2bd0c00ae7c6f0e51c810fda04
Albert <hermanlau89@gmail.com>
2020-11-03 15:40:38 +0800

    tambah Pegawai

--- Trial/FormTambahPegawai.Designer.cs
+++ Trial/FormTambahPegawai.Designer.cs
@@ -38,6 +38,10 @@
             this.label2 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPwd = new System.Windows.Forms.TextBox();
+            this.textBoxRePwd = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,9 +50,10 @@
             this.buttonKeluar.BackColor = System.Drawing.Color.RoyalBlue;
             this.buttonKeluar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonKeluar.Font = new System.Drawing.Font("Open Sans Semibold", 12F);
-            this.buttonKeluar.Location = new System.Drawing.Point(122, 156);
+            this.buttonKeluar.Location = new System.Drawing.Point(294, 261);
+            this.buttonKeluar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonKeluar.Name = "buttonKeluar";
-            this.buttonKeluar.Size = new System.Drawing.Size(95, 36);
+            this.buttonKeluar.Size = new System.Drawing.Size(127, 44);
             this.buttonKeluar.TabIndex = 6;
             this.buttonKeluar.Text = "KELUAR";
             this.buttonKeluar.UseVisualStyleBackColor = false;
@@ -58,9 +63,10 @@
             this.buttonSimpan.BackColor = System.Drawing.Color.RoyalBlue;
             this.buttonSimpan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonSimpan.Font = new System.Drawing.Font("Open Sans Semibold", 12F);
-            this.buttonSimpan.Location = new System.Drawing.Point(240, 156);
+            this.buttonSimpan.Location = new System.Drawing.Point(465, 261);
+            this.buttonSimpan.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonSimpan.Name = "buttonSimpan";
-            this.buttonSimpan.Size = new System.Drawing.Size(95, 36);
+            this.buttonSimpan.Size = new System.Drawing.Size(127, 44);
             this.buttonSimpan.TabIndex = 5;
             this.buttonSimpan.Text = "SIMPAN";
             this.buttonSimpan.UseVisualStyleBackColor = false;
@@ -68,91 +74,141 @@
             // 
             // textBoxJenisBarang
             // 
-            this.textBoxJenisBarang.Location = new System.Drawing.Point(122, 68);
+            this.textBoxJenisBarang.Location = new System.Drawing.Point(311, 84);
+            this.textBoxJenisBarang.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxJenisBarang.Name = "textBoxJenisBarang";
-            this.textBoxJenisBarang.Size = new System.Drawing.Size(185, 20);
+            this.textBoxJenisBarang.Size = new System.Drawing.Size(245, 22);
             this.textBoxJenisBarang.TabIndex = 2;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Open Sans Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(14, 68);
+            this.label7.Location = new System.Drawing.Point(19, 84);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(51, 22);
+            this.label7.Size = new System.Drawing.Size(65, 27);
             this.label7.TabIndex = 9;
             this.label7.Text = "Role :";
             // 
             // textBoxIDPenjual
             // 
-            this.textBoxIDPenjual.Location = new System.Drawing.Point(122, 42);
+            this.textBoxIDPenjual.Location = new System.Drawing.Point(311, 52);
+            this.textBoxIDPenjual.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxIDPenjual.Name = "textBoxIDPenjual";
-            this.textBoxIDPenjual.Size = new System.Drawing.Size(185, 20);
+            this.textBoxIDPenjual.Size = new System.Drawing.Size(245, 22);
             this.textBoxIDPenjual.TabIndex = 1;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Open Sans Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(14, 42);
+            this.label6.Location = new System.Drawing.Point(19, 52);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(64, 22);
+            this.label6.Size = new System.Drawing.Size(81, 27);
             this.label6.TabIndex = 8;
             this.label6.Text = "Nama :";
             // 
             // textBoxIDBarang
             // 
-            this.textBoxIDBarang.Location = new System.Drawing.Point(122, 16);
+            this.textBoxIDBarang.Location = new System.Drawing.Point(311, 20);
+            this.textBoxIDBarang.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxIDBarang.Name = "textBoxIDBarang";
-            this.textBoxIDBarang.Size = new System.Drawing.Size(185, 20);
+            this.textBoxIDBarang.Size = new System.Drawing.Size(245, 22);
             this.textBoxIDBarang.TabIndex = 0;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Open Sans Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(14, 16);
+            this.label2.Location = new System.Drawing.Point(19, 20);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 22);
+            this.label2.Size = new System.Drawing.Size(43, 27);
             this.label2.TabIndex = 7;
             this.label2.Text = "ID :";
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBoxRePwd);
+            this.panel1.Controls.Add(this.textBoxPwd);
             this.panel1.Controls.Add(this.textBoxJenisBarang);
             this.panel1.Controls.Add(this.label7);
             this.panel1.Controls.Add(this.textBoxIDPenjual);
             this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.textBoxIDBarang);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(10, 45);
+            this.panel1.Location = new System.Drawing.Point(13, 55);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(324, 105);
+            this.panel1.Size = new System.Drawing.Size(579, 198);
             this.panel1.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.BackColor = System.Drawing.Color.Gainsboro;
             this.label1.Font = new System.Drawing.Font("Open Sans Semibold", 14.25F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(10, 7);
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(324, 33);
+            this.label1.Size = new System.Drawing.Size(579, 41);
             this.label1.TabIndex = 7;
             this.label1.Text = "TAMBAH PEGAWAI";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // textBoxPwd
+            // 
+            this.textBoxPwd.Location = new System.Drawing.Point(311, 123);
+            this.textBoxPwd.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxPwd.Name = "textBoxPwd";
+            this.textBoxPwd.Size = new System.Drawing.Size(245, 22);
+            this.textBoxPwd.TabIndex = 10;
+            // 
+            // textBoxRePwd
+            // 
+            this.textBoxRePwd.Location = new System.Drawing.Point(311, 153);
+            this.textBoxRePwd.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxRePwd.Name = "textBoxRePwd";
+            this.textBoxRePwd.Size = new System.Drawing.Size(245, 22);
+            this.textBoxRePwd.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Open Sans Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(19, 111);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 27);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Password:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Open Sans Semibold", 10F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(19, 148);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(245, 23);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Masukan Kembali Password:";
+            // 
             // FormTambahPegawai
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
-            this.ClientSize = new System.Drawing.Size(347, 200);
+            this.ClientSize = new System.Drawing.Size(605, 318);
             this.Controls.Add(this.buttonKeluar);
             this.Controls.Add(this.buttonSimpan);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormTambahPegawai";
             this.Text = "FormTambahPegawai";
             this.panel1.ResumeLayout(false);
@@ -173,5 +229,9 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxRePwd;
+        private System.Windows.Forms.TextBox textBoxPwd;
     }
 }
